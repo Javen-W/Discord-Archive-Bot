a439e37593f208526a41955519ebadc995d601d8
javenzamojcin <javen.zamojcin@berkshiregrey.com>
2023-08-08 09:49:30 -0400

    impl Bot base class

--- bot.py
+++ bot.py
@@ -1,24 +1,27 @@
 import logging
 import discord
+from discord.ext.commands import bot, command
 
 
-class Bot(discord.Client):
+class Bot(discord.ext.commands.Bot):
     def __init__(self, token: str):
         # define intents
         intents = discord.Intents.default()
         intents.message_content = True
 
         # init base client class
-        super().__init__(intents=intents)
+        super().__init__(command_prefix="!", intents=intents)
 
         # run client
         super().run(token=token)
 
     async def on_ready(self):
-        logging.info(f"Ready from f{self.user}!")
+        logging.info(f"Ready from {self.user}!")
 
     async def on_message(self, message):
+        logging.info("on_message()")
         if message.author == self.user:
             return
         if message.content.startswith("$hello"):
             await message.channel.send("Hello!")
+        logging.info(message.channel)
